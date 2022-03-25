35948b491cb7ed9ad312c463f6cc9fb6a1e322ed
Max Berlinski <maxberlinski@Maxs-iMac-Pro.local>
2022-03-25 14:53:45 +0100

    initial setup for ios build

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:karpportal/SplashScreen.dart';
+import 'firebase_options.dart';
 
 import 'HomeScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
 
   runApp(const MyApp());
 }
