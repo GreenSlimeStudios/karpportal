070c6ced7bd0bcec9976b46dcc71bf452ebb68bd
Max Berliński <maxber.gdynia@gmail.com>
2022-07-10 12:17:28 +0200

    reading drom database

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,6 +13,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final ScrollController _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +62,70 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  Widget content() => SliverToBoxAdapter(child: Container(height: 1000));
+  Widget content() => SliverToBoxAdapter(
+        child: Container(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection("Posts")
+                .orderBy('timeMil', descending: true)
+                .snapshots(),
+            builder: (context, snapshot) {
+              print(snapshot);
+              print(snapshot.data);
+              print("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+              return (snapshot.connectionState == ConnectionState.waiting)
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Container(
+                      constraints: BoxConstraints(
+                        maxHeight: 1000,
+                        minHeight: 1,
+                      ),
+                      child: ListView.builder(
+                        reverse: true,
+                        controller: _scrollController,
+                        itemCount: snapshot.data!.docs.length,
+                        // itemCount: 2,
+
+                        itemBuilder: (context, index) {
+                          print(snapshot.data);
+                          print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+                          var data = snapshot.data!.docs[index].data() as Map<String, dynamic>;
+                          if (data != null) {
+                            // return (data['authorID'] != globals.myUser!.uid)
+                            return Container(
+                              margin: const EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    data["title"],
+                                  ),
+                                  SizedBox(height: 300),
+                                  // Text(
+                                  //   data["content"],
+                                  // ),
+                                  // Text(
+                                  //   data["authorID"],
+                                  // ),
+                                ],
+                              ),
+                            );
+                            // : Container();
+                          } else {
+                            return CircularProgressIndicator();
+                          }
+                        },
+                      ),
+                    );
+            },
+          ),
+        ),
+      );
   void createPost() async {
     var info = await Navigator.push(
         context, MaterialPageRoute(builder: (context) => const CreatePostPage()));
