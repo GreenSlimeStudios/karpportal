f4db6acfbce69105ede96f0d91808a53442dd9fb
Max Berliński <maxber.gdynia@gmail.com>
2022-08-06 22:33:25 +0200

    removed secrets

--- lib/secrets/apiKeys.dart
+++
@@ -1,7 +0,0 @@
-class Secrets {
-  String cloudMessagingApiKey =
-      "key=AAAA9xPglTQ:APA91bEuI1Hg2Mw6dLpBuh2bDvJfgcYOUm_rEUhq3glaPRzICYtTUQEG6iFF1r_EeWx3B_wC9sTDVxk0x1PYgcSh-N9Di4qG-GNF3LVDjhc9F5B_cfEqvdky-Rc1ILwdAc1oqtB5Ho8v";
-  String fcmApiKey =
-      "key=AAAASkfbv0c:APA91bGlzPWhK6D07chlvcnpD6KYZUPt9Xeif4QbadV-Y8QJwlG7qI_YmmFtTooliqjQET5YCXBs4B9GFtkGco7GKntJZeve2LfMeIAtNobDym3-4AxJhPYF25Xz8WOvprQUmDKwIpsU";
-  String firebaseAuthApiKey = 'AIzaSyA151Up5KqDmbiKEub8g6WzwIOZZc4HgDA';
-}
